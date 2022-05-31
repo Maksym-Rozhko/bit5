c4b5564014f426bba8348decaadb44e794a41ce8
Maksym <maksymRozhko7@gmail.com>
2022-05-31 13:23:57 +0300

    init: project structure, add files

---
+++ README.md
@@ -0,0 +1,4 @@
+# Bit5
+## We are a team of crypto-industry professionals. 
+## We have developed a platform for launching new cryptocurrency tokens together with the community.
+## Join the winning team and buy a new token at the lowest price!
---
+++ src/js/components/initSliders.js
@@ -0,0 +1,62 @@
+import Swiper, { Navigation, Pagination } from 'swiper';
+Swiper.use([Navigation, Pagination]);
+
+new Swiper('.swiper.main-slider', {
+    slidesPerView: 1,
+    loop: true,
+
+    navigation: {
+        nextEl: '.main-slider .swiper-button-next',
+        prevEl: '.main-slider .swiper-button-prev',
+    },
+});
+
+new Swiper('.swiper.projects-slider', {
+    slidesPerView: 4,
+    spaceBetween: 20,
+
+    pagination: {
+        el: '.swiper-pagination',
+    },
+
+    breakpoints: {
+        320: {
+          slidesPerView: 1,
+          spaceBetween: 10,
+        },
+        575: {
+          slidesPerView: 2,
+        },
+        1024: {
+          slidesPerView: 3,
+        },
+        1440: {
+          slidesPerView: 4,
+        }
+    },
+});
+
+new Swiper('.swiper.news-slider', {
+    slidesPerView: 4,
+    spaceBetween: 20,
+
+    pagination: {
+        el: '.swiper-pagination',
+    },
+
+    breakpoints: {
+        320: {
+          slidesPerView: 1,
+          spaceBetween: 10,
+        },
+        575: {
+          slidesPerView: 2,
+        },
+        1024: {
+          slidesPerView: 3,
+        },
+        1440: {
+          slidesPerView: 4,
+        }
+    },
+});

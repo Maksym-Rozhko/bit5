58cf1d88c210cb7ab5fc87d36c97986253c0d18b
Maksym <maksymRozhko7@gmail.com>
2022-05-31 23:01:39 +0300

    feat: preloader

--- src/js/_components.js
+++ src/js/_components.js
@@ -1,3 +1,4 @@
+import preloader from './components/preloader';
 import cursor from './components/cursor';
 import initSliders from './components/initSliders';
 import headerScrollFixed from './components/fixedHeader';

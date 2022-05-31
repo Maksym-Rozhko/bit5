9a3e81812e01ca7660afba352d52291d305db2fa
Maksym <maksymRozhko7@gmail.com>
2022-05-31 20:53:09 +0300

    feat: timer, main screen, custom cursor

--- src/js/_components.js
+++ src/js/_components.js
@@ -1,4 +1,6 @@
+import cursor from './components/cursor';
 import initSliders from './components/initSliders';
 import headerScrollFixed from './components/fixedHeader';
 import scrollToTop from './components/scrollToTop';
 import customSelect from './components/customSelect';
+import timer from './components/timer';

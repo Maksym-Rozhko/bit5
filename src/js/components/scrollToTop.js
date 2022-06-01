628e7f488a107122c93c68ec1dcbe94baaac6a72
Maksym <maksymRozhko7@gmail.com>
2022-06-01 21:32:24 +0300

    feat: footer, adaptive, scroll to top

--- src/js/components/scrollToTop.js
+++ src/js/components/scrollToTop.js
@@ -2,13 +2,15 @@ const btnScrollUp = document.querySelector('.scroll-top');
 const topElem = document.querySelector('.main'); 
 
 try {
+  window.addEventListener('scroll', () => {
+    let scrollY = window.scrollY;
+    scrollY > 1000 ? btnScrollUp.classList.remove('d-none') : btnScrollUp.classList.add('d-none');
+  });
+
   btnScrollUp.addEventListener('click', () => {
     topElem.scrollIntoView({
       behavior: 'smooth',
       block: 'start'
     });
-  //   jQuery("html, body").animate({
-  //     scrollTop: "0"
-  // }, 700);
   });
 } catch (error) {}

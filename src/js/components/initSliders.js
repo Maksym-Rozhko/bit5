75ec11a24f52c78d5c8e6b66c33303b279263e68
Maksym <maksymRozhko7@gmail.com>
2022-06-20 12:44:19 +0300

    feat: new page roadmp, init roadmap slider

--- src/js/components/initSliders.js
+++ src/js/components/initSliders.js
@@ -1,10 +1,11 @@
-import Swiper, { Navigation, Pagination } from 'swiper';
-Swiper.use([Navigation, Pagination]);
+import Swiper, { Navigation, Pagination, Scrollbar, Mousewheel } from 'swiper';
+Swiper.use([Navigation, Pagination, Scrollbar, Mousewheel]);
 
-new Swiper('.swiper.rating__slider', {
+const swiper = new Swiper('.swiper.rating__slider', {
     slidesPerView: 4.5,
     spaceBetween: 20,
-    loop: true,
+    init: true,
+    observer: true,
 
     navigation: {
         nextEl: '.slider__controls .rating__btns.swiper-button-next',
@@ -30,6 +31,34 @@ new Swiper('.swiper.rating__slider', {
       1440: {
         slidesPerView: 4.5,
         spaceBetween: 20,
+      }
+  },
+});
+
+new Swiper('.swiper.roadmap__slider', {
+    slidesPerView: 3,
+    spaceBetween: 115,
+    speed: 500,
+    mousewheel: true,
+
+    scrollbar: {
+      el: '.swiper-scrollbar',
+      draggable: true,
+      // hide: false,
+    },
+
+    breakpoints: {
+      320: {
+        slidesPerView: 1,
+        spaceBetween: 10,
+      },
+      1024: {
+        slidesPerView: 2,
+        spaceBetween: 115,
+      },
+      1440: {
+        slidesPerView: 3,
+        spaceBetween: 115,
       }
   },
 });
